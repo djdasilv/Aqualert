0bc1292e416e7bfe568b64d2c3a3b8d538cb330e
Daniel da silva <daniel.dasilva@epfl.ch>
2024-12-03 12:55:06 +0100

    EasyButton library implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 
-#include <SD.h>
+//#include <SD.h>
 #include <TFT_eSPI.h>
 #include "interface.h"
-#include <pgmspace.h>
+//#include <pgmspace.h>
+#include "EasyButton.h"
 
 #define PEOPLE_MODE 1
 #define VULNERABLE_MODE 2
 #define WATER_LEVEL 3
+#define HOLD_DURATION 2000
 
+EasyButton UpButton(UP_BUTTON);
+EasyButton OkButton(OK_BUTTON);
 
 // TFT display object
 TFT_eSPI tft = TFT_eSPI();
@@ -16,11 +20,11 @@ TFT_eSPI tft = TFT_eSPI();
 const int imageWidth = 320;
 const int imageHeight = 240;
 
-void IRAM_ATTR handleUpButtonPress() {
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
+void handleUpButtonPress() {
+  //static unsigned long last_interrupt_time = 0;
+  //unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  if (interrupt_time - last_interrupt_time > 200) {
+  //if (interrupt_time - last_interrupt_time > 200) {
     switch (getMode()) {
       case 1:
         setPeople(getPeople()+1);
@@ -38,26 +42,30 @@ void IRAM_ATTR handleUpButtonPress() {
           setLevel(getLevel()+1);
           break;}
     }
-    }
-  last_interrupt_time = interrupt_time;
+  //  }
+  //last_interrupt_time = interrupt_time;
 }
 
-void IRAM_ATTR handleOkButtonPress() {
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
+void onHoldCallback(){
+  if (getMode()==4) setMode(PEOPLE_MODE);
+  
+}
+
+void handleOkButtonPress() {
+  //static unsigned long last_interrupt_time = 0;
+  //unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  if (interrupt_time - last_interrupt_time > 200) {
+  //if (interrupt_time - last_interrupt_time > 200) {
       clearOldRect(tft);
       drawText(tft);
-      
       if (getMode()<WATER_LEVEL) {
         setMode(getMode()+1);
       } else {
         sendInfo();
         setMode(4);
       }
-  }
-  last_interrupt_time = interrupt_time;
+  //}
+  //last_interrupt_time = interrupt_time;
 }
 
 
@@ -66,15 +74,16 @@ void setup() {
   tft.begin();
   tft.fillScreen(TFT_WHITE); // Fill the screen with black at the start
   tft.setRotation(1);  // Set rotation to match your display orientation
-  //Configure button pins as inputs
-  pinMode(UP_BUTTON, INPUT);
-  //pinMode(DOWN_BUTTON, INPUT);// Problème car 21 pour la backlight
-  pinMode(OK_BUTTON, INPUT); 
 
-  // Attach interrupts to the button pins
-  attachInterrupt(digitalPinToInterrupt(UP_BUTTON), handleUpButtonPress, RISING);
-  //attachInterrupt(digitalPinToInterrupt(DOWN_BUTTON), handleDownButtonPress, FALLING);
-  attachInterrupt(digitalPinToInterrupt(OK_BUTTON), handleOkButtonPress, RISING);
+  //Configure button pins as inputs
+  OkButton.begin();
+  UpButton.begin();
+
+  OkButton.onPressedFor(HOLD_DURATION, onHoldCallback);
+  UpButton.onPressedFor(HOLD_DURATION, onHoldCallback);
+
+  UpButton.onPressed(handleUpButtonPress);
+  OkButton.onPressed(handleOkButtonPress);
 
   // Draw the background image
   tft.pushImage(0,0,imageWidth,imageHeight,image);
@@ -82,7 +91,9 @@ void setup() {
 
 uint8_t mode_copy;
 void loop() {
-  if(getMode()<=3){
+  OkButton.read();
+  UpButton.read();
+  if(getMode()<=WATER_LEVEL){
     blinkingSquare(tft);
     drawText(tft);
   } else if (getMode()==4){
