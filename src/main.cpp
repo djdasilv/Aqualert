43dd9d5094d441859d3ad476aa3ba63c87455e23
Daniel da silva <daniel.dasilva@epfl.ch>
2024-12-04 14:41:08 +0100

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 
-//#include <SD.h>
 #include <TFT_eSPI.h>
 #include "interface.h"
-//#include <pgmspace.h>
 #include "EasyButton.h"
 
 #define PEOPLE_MODE 1
 #define VULNERABLE_MODE 2
 #define WATER_LEVEL 3
-#define HOLD_DURATION 2000
+#define HOLD_DURATION 5000
 
 EasyButton UpButton(UP_BUTTON);
 EasyButton OkButton(OK_BUTTON);
@@ -16,15 +14,12 @@ EasyButton OkButton(OK_BUTTON);
 // TFT display object
 TFT_eSPI tft = TFT_eSPI();
 
-// Image dimensions (replace with your actual dimensions)
+// Image dimensions
 const int imageWidth = 320;
 const int imageHeight = 240;
 
+//Callback function for when up button is pressed
 void handleUpButtonPress() {
-  //static unsigned long last_interrupt_time = 0;
-  //unsigned long interrupt_time = millis();
-  // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  //if (interrupt_time - last_interrupt_time > 200) {
     switch (getMode()) {
       case 1:
         setPeople(getPeople()+1);
@@ -42,20 +37,18 @@ void handleUpButtonPress() {
           setLevel(getLevel()+1);
           break;}
     }
-  //  }
-  //last_interrupt_time = interrupt_time;
 }
 
-void onHoldCallback(){
-  if (getMode()==4) setMode(PEOPLE_MODE);
+//Callback function for when any button is pressed
+void onHoldAnyCallback(){
+  if (getMode()==4) {
+    tft.pushImage(0,0,imageWidth,imageHeight,image);
+    setMode(PEOPLE_MODE);
+  }
   
 }
-
+//Callback function for when ok button is pressed
 void handleOkButtonPress() {
-  //static unsigned long last_interrupt_time = 0;
-  //unsigned long interrupt_time = millis();
-  // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  //if (interrupt_time - last_interrupt_time > 200) {
       clearOldRect(tft);
       drawText(tft);
       if (getMode()<WATER_LEVEL) {
@@ -64,10 +57,9 @@ void handleOkButtonPress() {
         sendInfo();
         setMode(4);
       }
-  //}
-  //last_interrupt_time = interrupt_time;
 }
 
+void deepSleep(){}
 
 void setup() {
   // Initialize TFT display
@@ -79,8 +71,8 @@ void setup() {
   OkButton.begin();
   UpButton.begin();
 
-  OkButton.onPressedFor(HOLD_DURATION, onHoldCallback);
-  UpButton.onPressedFor(HOLD_DURATION, onHoldCallback);
+  OkButton.onPressedFor(HOLD_DURATION, onHoldAnyCallback);
+  UpButton.onPressedFor(HOLD_DURATION, onHoldAnyCallback);
 
   UpButton.onPressed(handleUpButtonPress);
   OkButton.onPressed(handleOkButtonPress);
@@ -89,7 +81,6 @@ void setup() {
   tft.pushImage(0,0,imageWidth,imageHeight,image);
 }
 
-uint8_t mode_copy;
 void loop() {
   OkButton.read();
   UpButton.read();
