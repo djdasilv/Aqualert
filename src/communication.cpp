5789684211f2ecc6cb826f6c9c25972ab3bdcf3e
Daniel da silva <daniel.dasilva@epfl.ch>
2024-12-13 23:27:26 +0100

    Minor changes 99% complete

--- src/communication.cpp
+++ src/communication.cpp
@@ -55,27 +55,48 @@ void setMode(uint8_t amount){
 }
 
 void lora_begin(){
+  //initialize Serial Monitor
   Serial.begin(115200);
   while (!Serial);
   Serial.println("LoRa Sender");
+
   //setup LoRa transceiver module
-  LoRa.setPins(5, 14, 2); 
-  while (!LoRa.begin(868E6)) { //868E6 for Europe
+  LoRa.setPins(5,14,2);
+  LoRa.setTxPower(20);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(7.8E3);
+  LoRa.setCodingRate4(8);
+  LoRa.enableCrc();
+  LoRa.setGain(6);
+
+  while (!LoRa.begin(868E6)) {
     Serial.println(".");
     delay(500);
   }
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(8);
-  LoRa.enableCrc();
-  LoRa.setSyncWord(0xF3);
-  LoRa.setTxPower(20,PA_OUTPUT_PA_BOOST_PIN);
-
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF1);
   Serial.println("LoRa Initializing OK!");
 }
 
 void sendInfo(){
+  RTC_DATA_ATTR static int counter=0;
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
   LoRa.beginPacket();
-  LoRa.print("ID:"+ String(DEVICE_ID)+" "+String(getPeople())+String(getVulnerable())+String(getLevel()));
+  LoRa.print("ID "+ String(DEVICE_ID)+
+  " ; Loc [46.520402, 6.564847]"+
+  " ; Answer ["+ String(getPeople())+","+String(getVulnerable())+","+String(getLevel())+
+  "] ; Counter:"+ counter); 
+  LoRa.endPacket();
+  counter++;
+}
+
+void sendReset(){
+  LoRa.beginPacket();
+  LoRa.print("ID "+ String(DEVICE_ID)+" ; Loc [46.520402, 6.564847] ; Answer [0,0,0] ; Counter:");
   LoRa.endPacket();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include "LoRa.h"
 #include <esp_sleep.h>
 #include "Button2.h"
+#include <esp32/rom/rtc.h>
+
 
 #define PEOPLE_MODE 1
 #define VULNERABLE_MODE 2
 #define WATER_LEVEL 3
 #define SEND_INFO 4
-#define HOLD_DURATION 5000
+#define HOLD_DURATION 2000
 
 Button2 UpButton;
 Button2 OkButton;
@@ -22,9 +24,19 @@ bool send=false;
 #define WAKEUP_GPIO_27              GPIO_NUM_27     // Only RTC IO are allowed - ESP32 Pin example
 uint64_t bitmask =  BUTTON_PIN_BITMASK(WAKEUP_GPIO_27);
 
+RTC_DATA_ATTR bool firstSend= true;
+RTC_DATA_ATTR uint16_t counter= 0;
+
+void clearRTC() {
+  volatile uint32_t* rtc_mem = (uint32_t*) 0x50000000; // RTC slow memory base address
+  for (int i = 0; i < 128; i++) {
+    rtc_mem[i] = 0; // Clear 512 bytes (128 * 4 bytes)
+  }
+}
+
+
 //Callback function for when up button is pressed
 void handleUpButtonPress(Button2& b) {
-    if (b==OkButton);
     timeButtonUp = millis();
     switch (getMode()) {
       case PEOPLE_MODE:
@@ -50,23 +62,29 @@ void handleUpButtonPress(Button2& b) {
 
 //Callback function for when any button is pressed
 void onHoldAnyCallback(Button2& b){
-  if (b==OkButton);
+  if (b.getPin()==OK_BUTTON) timeButtonOk = millis();
+  if (b.getPin()==UP_BUTTON) timeButtonUp = millis();
   if (getMode()==4) {
     setMode(1);
+    send=false;
   }
 }
 
 //Callback function for when ok button is pressed
 void handleOkButtonPress(Button2& b) {
-      if (b==OkButton);
       timeButtonOk = millis();
       if (getMode()<WATER_LEVEL) {
         setMode(getMode()+1);
+      } else if (getMode()==WATER_LEVEL and firstSend==true) {
+        setMode(4);
+        Serial.println("Sending info "+ String(send));
+        send=true;
+        firstSend= false;
+        sendInfo();
       } else {
         setMode(4);
-        Serial.println("Sending info"+ String(send));
-        send=true;  // Call your 5-minute task
-        //timerAlarmEnable(My_timer);
+        Serial.println("Sending info "+ String(send));
+        send=true;
       }
       Serial.println("Next Mode: "+ String(getMode()));
 }
@@ -90,12 +108,13 @@ void setup() {
   esp_sleep_enable_ext1_wakeup(bitmask, ESP_EXT1_WAKEUP_ANY_HIGH);
 
   const uint64_t uS_TO_S_FACTOR = 1000000; // Conversion factor
-  const uint64_t SLEEP_TIME = 300;        // 5 minutes
+  const uint64_t SLEEP_TIME = 10;        // 5 minutes
   esp_sleep_enable_timer_wakeup(SLEEP_TIME * uS_TO_S_FACTOR);
   esp_sleep_enable_ext1_wakeup(bitmask, ESP_EXT1_WAKEUP_ANY_HIGH);
   if(getMode()==SEND_INFO) send=true;
 }
- #define INTERVAL 10000
+
+#define INTERVAL 20000
 void loop() {
   OkButton.loop();
   UpButton.loop();
@@ -105,7 +124,15 @@ void loop() {
     Serial.println("Going to sleep");
     esp_deep_sleep_start();
   }
-  
+  if(min(millis()-timeButtonOk,millis()-timeButtonUp)>INTERVAL){
+    Serial.println("Going to sleep");
+    esp_deep_sleep_start();
+  }
+  if (OkButton.isPressed() && UpButton.isPressed()){
+    clearRTC();
+    esp_restart();
+  }
 }
+
 
 
